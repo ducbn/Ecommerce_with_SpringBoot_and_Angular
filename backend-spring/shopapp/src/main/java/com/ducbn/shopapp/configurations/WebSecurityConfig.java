f935eb285cd4b0c5b884ee58f251a3c221262060
Bui Ngoc Duc <ducbn@192.168.1.27.non-exists.ptr.local>
2025-04-13 02:32:07 +0700

    validate form register, custom CORS backend

--- backend-spring/shopapp/src/main/java/com/ducbn/shopapp/configurations/WebSecurityConfig.java
+++ backend-spring/shopapp/src/main/java/com/ducbn/shopapp/configurations/WebSecurityConfig.java
@@ -8,14 +8,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.util.Pair;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.*;
 
+@EnableWebMvc
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -72,7 +81,23 @@ public class WebSecurityConfig {
                             .requestMatchers(DELETE,
                                     String.format("%s/order_details/**", apiPrefix)).hasRole(Role.ADMIN)
                             .anyRequest().authenticated();
-                });
+                }).csrf(AbstractHttpConfigurer::disable);
+
+        http.cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedOrigins(List.of("*"));
+                corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                corsConfiguration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+                corsConfiguration.setExposedHeaders(List.of("x-auth-token"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", corsConfiguration);
+                httpSecurityCorsConfigurer.configurationSource(source);
+            }
+        });
+
+
         return http.build();
     }
 }
--- shopapp-angular/src/app/register/register.component.ts
+++ shopapp-angular/src/app/register/register.component.ts
@@ -1,46 +1,109 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Thêm import này
 import { FooterComponent } from '../footer/footer.component';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, FooterComponent],
+  imports: [
+    FormsModule, 
+    CommonModule, 
+    FooterComponent,
+    HttpClientModule, // Thêm để hỗ trợ HttpClient
+    RouterModule
+  ], 
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
+
   phone: string;
   password: string;
   retypePassword: string;
-  fullName:string;
+  fullName: string;
   address: string;
   isAccepted: boolean;
   dateOfBirth: Date;
 
-  constructor(){
-    this.phone = '';
-    this.password = '';
-    this.retypePassword = '';
-    this.fullName = '';
-    this.address ='';
+  constructor(private http: HttpClient, private router: Router) {
+    this.phone = '0987654321';
+    this.password = '12345678';
+    this.retypePassword = '12345678';
+    this.fullName = 'Bui Ngoc Duc';
+    this.address = 'Hai Duong';
     this.isAccepted = false;
     this.dateOfBirth = new Date();
-    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18)
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
   }
 
-  onPhoneChange(){
-    console.log(`Phone typed: ${this.phone}`)
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
   }
 
-  register(){
+  register() {
     const message = `phone: ${this.phone}` +
-                    `password: ${this.password}`+
-                    `retypePassword: ${this.retypePassword}`+
-                    `address: ${this.address}`+
-                    `fullName: ${this.fullName}`+
+                    `password: ${this.password}` +
+                    `retypePassword: ${this.retypePassword}` +
+                    `address: ${this.address}` +
+                    `fullName: ${this.fullName}` +
                     `isAccepted: ${this.isAccepted}`;
-    alert(message)
+    // alert(message);
+    const apiUrl = "http://localhost:8080/api/v1/users/register"
+    const registerData = {
+      "fullname": this.fullName,
+      "phone_number" : this.phone,
+      "address": this.address,
+      "password": this.password,
+      "retype_password": this.retypePassword,
+      "date_of_birth": this.dateOfBirth,
+      "facebook_account_id": 0,
+      "google_account_id": 0,
+      "role_id": 1
+    }
+    const headers =  new HttpHeaders({ 'Content-Type': 'application/json'})
+    this.http.post(apiUrl, registerData, {headers},)
+    .subscribe({
+      next: (response: any) => {
+        debugger
+        alert("Đăng ký thành công")
+        this.router.navigate(['/login'])
+      },
+      complete: () => {
+        debugger
+      },
+      error: (error: any) => {
+        //Xử lý lỗi nếu có
+        alert(`Cannot register, error: ${error.error}`)
+      }
+    })
+  }
+
+  checkPasswordsMatch(){
+    if (this.password !== this.retypePassword){
+      this.registerForm.form.controls['retypePassword'].setErrors({'passwordMismatch': true});
+    }else{
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  checkAge(){
+    if (this.dateOfBirth){
+      const today = new Date()
+      const birthDate = new Date(this.dateOfBirth)
+      let age = today.getFullYear() - birthDate.getFullYear()
+      const monthDiff = today.getMonth() - birthDate.getMonth()
+      if(monthDiff < 0 || (monthDiff == 0 && today.getDate() < birthDate.getDate())){
+        age--;
+      }
+      if (age < 18){
+        this.registerForm.form.controls['dateOfBirth'].setErrors({'invalidAge' : true})
+      }else{
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null)
+      }
+    }
   }
 }
